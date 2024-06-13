befa618fc2cc5db51fe05254f92bcbdf5fd2872b
Luke Pilon <luke.pilon@gmail.com>
2024-06-12 20:31:46 -0500

    adding angular material

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,37 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, RouterModule, Routes } from '@angular/router';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { patientDataReducer } from './app/store/patient-data.reducer';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { PatientDataEffects } from './app/store/patient-data.effects';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { PatientDashboardComponent } from './app/patient-dashboard/patient-dashboard.component';
+import { PatientDetailComponent } from './app/patient-detail/patient-detail.component';
+import { ReferralFormComponent } from './app/referral-form/referral-form.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
-bootstrapApplication(AppComponent, appConfig)
+const routes: Routes = [
+  {path: '', redirectTo: '/patients', pathMatch: 'full'},
+  {path: 'patients', component: PatientDashboardComponent},
+  {path: 'patient/:id', component: PatientDetailComponent},
+  {path: 'referral', component: ReferralFormComponent},
+];
+
+
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideStore({patientData: patientDataReducer}),
+    provideEffects([PatientDataEffects]),
+    importProvidersFrom(HttpClientModule), provideAnimationsAsync(),
+    importProvidersFrom(MatDialogModule, MatButtonModule)
+  ]
+})
   .catch((err) => console.error(err));
